29f822898a538bbc6277c4766c0baaa219afada5
Kyrill Willems <kyrill.willems@student.tsaam.be>
2026-01-12 12:03:11 +0100

    2

--- spelers/urls.py
+++ spelers/urls.py
@@ -2,11 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.test),
     path('speler/add/', views.newSpeler),
+    path('editSpelers/<int:id>', views.editSpeler),
+    path('deleteSpeler/', views.deleteSpeler),
     path('speler/<int:id>', views.spelerById),
     path('speler/', views.GegevensAlleSpelers),
     path('speler/names/', views.naamSpelers),
     path('addMatch/', views.newMatch),
+    path('editMatch/<int:id>', views.editMatch),
+    path('deleteMatch/', views.deleteMatch),
     path('matches/', views.allMatches),
+    path('matchPunten/resultaat/<int:idSpeler>/<int:matchCode>', views.puntenIdCode),
+    path('punten/<int:idSpeler>', views.puntenSpeler),
 ]
--- spelers/views.py
+++ spelers/views.py
@@ -6,10 +6,8 @@ from django.forms.models import model_to_dict
 from .models import speler, match_punten
 # Create your views here.
 
-def test(request):
-   return HttpResponse('Test')
-
-#spelers
+#spelers views:
+#een nieuwe speler aanmaken
 @csrf_exempt
 def newSpeler(request):
     post_data =  json.loads(request.body.decode('utf-8'))
@@ -20,19 +18,41 @@ def newSpeler(request):
     new.save()
     return JsonResponse(model_to_dict(new))
 
+#een speler aanpassen aan de hand van een id
+@csrf_exempt
+def editSpeler(request, id):
+    post_data =  json.loads(request.body.decode('utf-8'))
+    editS = speler.objects.get(pk = id)
+    editS.naam = post_data['naam']
+    editS.voornaam = post_data['voornaam']
+    editS.email = post_data['email']
+    editS.save()
+    return JsonResponse(model_to_dict(editS))
+
+#een speler verwijderd aan de hand van een id
+@csrf_exempt
+def deleteSpeler(request):
+    post_data =  json.loads(request.body.decode('utf-8'))
+    speler.objects.get(pk = post_data['id']).delete()
+    return JsonResponse({"Status":"Speler succesvol verwijderd!"})
+
+#een speler opzoeken aan de hand van een id, dit gebeurt in postman want anders is het niet veilig
 def spelerById(request, id):
     idSpeler = speler.objects.get(pk = id)
     return JsonResponse(model_to_dict(idSpeler))
 
+#alle spelers en de gegevens ervan tonen
 def GegevensAlleSpelers(request):
     all = speler.objects.values()
     return JsonResponse(list(all), safe=False)
 
+#alleeen de namen van alle spelers tonen
 def naamSpelers(request):
     naam = speler.objects.values('naam', 'voornaam')
     return JsonResponse(list(naam), safe=False)
 
-#match
+#match views:
+#een nieuwe match aanmaken
 @csrf_exempt
 def newMatch(request):
     post_data =  json.loads(request.body.decode('utf-8'))
@@ -43,7 +63,37 @@ def newMatch(request):
     newM.save()
     return JsonResponse(model_to_dict(newM))
 
+#een match aanpassen aan de hand van een id
+@csrf_exempt
+def editMatch(request, id):
+    post_data =  json.loads(request.body.decode('utf-8'))
+    editM = match_punten.objects.get(pk = id)
+    editM.nummerSpeler = post_data['nummerSpeler']
+    editM.punten = post_data['punten']
+    editM.matchCode = post_data['matchCode']
+    editM.save()
+    return JsonResponse(model_to_dict(editM))
+
+#een match verwijderen aan de hand van een id, dit gebeurt in postman want anders is het niet veilig
+@csrf_exempt
+def deleteMatch(request):
+    post_data =  json.loads(request.body.decode('utf-8'))
+    match_punten.objects.get(pk = post_data['id']).delete()
+    return JsonResponse({"Status":"Match succesvol verwijderd!"})
+
+#alle matches tonen
 def allMatches(request):
     all = match_punten.objects.values()
     return JsonResponse(list(all), safe=False)
+
+#de punten van een specieke match van een specifieke speler tonen
+def puntenIdCode(request, idSpeler, matchCode):
+    idCode = match_punten.objects.filter(nummerSpeler = idSpeler).filter(matchCode = matchCode).values('punten')
+    return JsonResponse(list(idCode), safe=False)
+
+#de alle punten van een specifieke speler tonen
+def puntenSpeler(request, idSpeler):
+    spelerId = match_punten.objects.filter(nummerSpeler = idSpeler).values('punten')
+    return JsonResponse(list(spelerId), safe=False)
+
   
